0861252dd6c1c7939048f580047a21b486ce2781
Kacper Marczewski <kacper.marczewski@gmail.com>
2021-10-16 17:17:55 +0200

    weight categories and sampling added

--- model.h
+++ model.h
@@ -9,60 +9,10 @@
 #include "boxer.h"
 #include "settings.h"
 
-float WD[] = {
-        0,
-        46.27,
-        47.63,
-        48.99,
-        50.80,
-        52.16,
-        53.52,
-        55.34,
-        57.15,
-        58.97,
-        61.23,
-        63.50,
-        66.68,
-        69.85,
-        72.57,
-        76.20,
-        79.38,
-        90.72,
-        100000
-};
-
-std::string WD_NAMES[] = {
-        "Atomweight",
-        "Strawweight",
-        "Junior flyweight",
-        "Flyweight",
-        "Junior bantamweight",
-        "Bantamweight",
-        "Junior featherweight",
-        "Featherweight",
-        "Junior lightweight",
-        "Lightweight",
-        "Super lightweight",
-        "Welterweight",
-        "Junior middleweight",
-        "Middleweight",
-        "Super middleweight",
-        "Light heavyweight",
-        "Cruiserweight",
-        "Heavyweight"
-};
-
-int calc_wd(struct boxer b) {
-    for (int i = 1; i < 19; i++) {
-        if (WD[i] >= b.val[WEIGHT] & WD[i - 1] < b.val[WEIGHT])
-            return i - 1;
-    }
-    return 0;
-}
-
 void model_round(struct boxer b[], struct settings s) {
     int round_len = std::stoi(s.val[ROUND_LEN]);
-    for (int i = 0; i < round_len; i++) {
+    int sampling = std::stoi(s.val[SAMPLING]);
+    for (int i = 0; i < round_len; i += sampling) {
         // TODO implement boxers actions and their impact
     }
 }
--- settings.h
+++ settings.h
@@ -11,11 +11,64 @@ struct settings {
 
 const std::string ROUND_LEN("round_len=");
 const std::string ROUNDS("rounds=");
+const std::string SAMPLING("sampling=");
 
 std::string PARAMS[] = {
         ROUND_LEN,
-        ROUNDS
+        ROUNDS,
+        SAMPLING
 };
+
+float WD[] = {
+        0,
+        46.27,
+        47.63,
+        48.99,
+        50.80,
+        52.16,
+        53.52,
+        55.34,
+        57.15,
+        58.97,
+        61.23,
+        63.50,
+        66.68,
+        69.85,
+        72.57,
+        76.20,
+        79.38,
+        90.72,
+        100000
+};
+
+std::string WD_NAMES[] = {
+        "Atomweight",
+        "Strawweight",
+        "Junior flyweight",
+        "Flyweight",
+        "Junior bantamweight",
+        "Bantamweight",
+        "Junior featherweight",
+        "Featherweight",
+        "Junior lightweight",
+        "Lightweight",
+        "Super lightweight",
+        "Welterweight",
+        "Junior middleweight",
+        "Middleweight",
+        "Super middleweight",
+        "Light heavyweight",
+        "Cruiserweight",
+        "Heavyweight"
+};
+
+int calc_wd(struct boxer b) {
+    for (int i = 1; i < 19; i++) {
+        if (WD[i] >= b.val[WEIGHT] & WD[i - 1] < b.val[WEIGHT])
+            return i - 1;
+    }
+    return 0;
+}
 
 void parse_settings_line(const std::string& line, struct settings *s) {
     for (auto & i : PARAMS) {
